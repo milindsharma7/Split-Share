d6733f706ae155cfe07cf11c499ed90cd26e8430
Milind <milindsharmagithub@gmail.com>
2023-07-21 23:19:13 +0530

    Home Screen text correction

--- split-share/src/pages/Main.jsx
+++ split-share/src/pages/Main.jsx
@@ -28,7 +28,7 @@ function Main() {
                     Visit the Working section to see the implementation.
                 </p>
                 <p>
-                    For more details and in depth analysis of the Debt Sharing problem please visit
+                    For more details and in depth analysis of the Debt Sharing problem please
                     refer <Link to='https://medium.com/@mithunmk93/algorithm-behind-splitwises-debt-simplification-feature-8ac485e97688' target='_blank'>THIS</Link> amazing blog.
                 </p>
                 <p>
